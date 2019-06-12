8e302c721a33377b215cd86c529ed6b403476fe2
tian shui <794064979@qq.com>
2019-06-12 17:32:51 +0800

    add update the state of points

--- game_of_life.py
+++ game_of_life.py
@@ -6,6 +6,12 @@ Created on Wed Jun 12 12:30:46 2019
 """
 
 import numpy as np
+from random import choice
+from copy import deepcopy
+from time import sleep
+from tkinter import *
+mygui = Tk()
+mainloop()
 
 #input_1 = np.zeros((5,5))
 #input_1[2,2] = 1
@@ -22,8 +28,10 @@ import numpy as np
 #        number += input_1[i+1,j-1]
 #        number += input_1[i+1,j]
 #        number += input_1[i+1,j+1]
-worldHigh=400
-worldWidth=400
+worldHigh = 5
+worldWidth= 5
+aliveNumber = 3
+deadNumber = 2
 class Point:
     def __init__(self,position):
         self.pos = position
@@ -36,15 +44,58 @@ class Point:
         
 class World:
     def __init__(self):
-        world=[]
         self.world=self.initialWorld()
+        self.initAlivePoint()
+        
+    def initialWorld(self):
+        world = []
         for pos_x in range(worldHigh):
             column=[]
             for pos_y in range(worldWidth):
                 column.append(Point((pos_x,pos_y)))
             world.append(column)
         return world
-          
+    
+    def initAlivePoint(self):
+        for high in self.world:
+            for point in high:
+                if choice((0,1)) == 0:
+                    continue
+                point.setAlive()
+
+    def count(self, point_obj):
+        number = 0
+        for x in range(-1,2):
+            for y in range(-1,2):
+                point_x = point_obj.x + x
+                point_y = point_obj.y + y
+                if ((point_x,point_y) == point_obj.pos) or \
+                ((point_x < 0) or (point_x >= worldHigh)) or \
+                ((point_y < 0) or (point_y >= worldWidth)):
+                    continue
+                print(point_obj.x,point_x)
+                print(point_obj.y,point_y)
+                if self.world[point_x][point_y].isAlive:
+                    number += 1
+        return number
+    
+    def gameProcess(self):
+        new_world = deepcopy(self.world)
+        for x, widelist in enumerate(self.world):
+            for y, _ in enumerate(widelist):
+                current_point = self.world[x][y]
+                alive_num = self.count(current_point)
+                if alive_num == aliveNumber:
+                    current_point.setAlive()
+                elif alive_num != deadNumber:
+                    current_point.setDead()
+        self.world = new_world
+        sleep(0.2)
+            
+if __name__ == '__main__':
+    current_world = World()
+    current_world.gameProcess()
+      
         
 # def getNeighbours(self, input_1): 
 #    alive_count = 0
