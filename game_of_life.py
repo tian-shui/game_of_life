5bab93ec19082f0333b752e34658401a22b1e868
tian shui <794064979@qq.com>
2019-06-13 10:42:04 +0800

    add interface and dynamic

--- game_of_life.py
+++ game_of_life.py
@@ -9,9 +9,8 @@ import numpy as np
 from random import choice
 from copy import deepcopy
 from time import sleep
+import tkinter as tk
 from tkinter import *
-mygui = Tk()
-mainloop()
 
 #input_1 = np.zeros((5,5))
 #input_1[2,2] = 1
@@ -28,8 +27,8 @@ mainloop()
 #        number += input_1[i+1,j-1]
 #        number += input_1[i+1,j]
 #        number += input_1[i+1,j+1]
-worldHigh = 5
-worldWidth= 5
+worldHigh = 10
+worldWidth= 10
 aliveNumber = 3
 deadNumber = 2
 class Point:
@@ -41,6 +40,11 @@ class Point:
         self.isAlive=True
     def setDead(self):
         self.isAlive=False
+    def display(self):
+        if self.isAlive:
+            return '*'
+        else:
+            return ' '
         
 class World:
     def __init__(self):
@@ -73,42 +77,84 @@ class World:
                 ((point_x < 0) or (point_x >= worldHigh)) or \
                 ((point_y < 0) or (point_y >= worldWidth)):
                     continue
-                print(point_obj.x,point_x)
-                print(point_obj.y,point_y)
+
                 if self.world[point_x][point_y].isAlive:
                     number += 1
         return number
     
+    def display(self):
+        if self.isAlive:
+            return '*'
+        else:
+            return ' '
+    
     def gameProcess(self):
         new_world = deepcopy(self.world)
-        for x, widelist in enumerate(self.world):
+        displist = []
+        for x, widelist in enumerate(new_world):
+            row = []
             for y, _ in enumerate(widelist):
-                current_point = self.world[x][y]
+                current_point = new_world[x][y]
                 alive_num = self.count(current_point)
                 if alive_num == aliveNumber:
                     current_point.setAlive()
                 elif alive_num != deadNumber:
                     current_point.setDead()
+                    
+                if current_point.isAlive:
+                    row.append(1)
+                else:
+                    row.append(0)
+            displist.append(row)
         self.world = new_world
-        sleep(0.2)
-            
+        return displist
+       # sleep(0.2)
+
+def cellDisplay(displist):
+    width = 50
+    for i in range(0,10):
+        pos_x = i*width
+        for j in range(0,10):
+            pos_y = j*width
+            canvas.create_rectangle(pos_x,pos_y,pos_x + width, pos_y + width, 
+                                    fill='white')
+    
+    for row in range(len(displist)):
+        row_x = row*width
+        for col in range(len(displist)):
+            col_y = col*width
+            if displist[row][col] == 1:
+                canvas.create_rectangle(col_y,row_x,col_y + width,row_x + width, 
+                                         fill='black')    
+                
 if __name__ == '__main__':
     current_world = World()
-    current_world.gameProcess()
+    #displist = current_world.gameProcess()
+    window = tk.Tk()
+    canvas = tk.Canvas(window, height=500, width=500)
+    canvas.pack()
+    looptimes=10
+    aa=[]
+    for times in range(looptimes):
+        displist = current_world.gameProcess()
+        aa.append(displist)
+        cellDisplay(displist)
+        window.update()
+        sleep(1)
+    
+    btn = tk.Button(window,text='Start')
+    btn.pack()
+    tk.mainloop() 
+    
+    
+#    listb = tk.Listbox(window)          #  创建两个列表组件
+#    for item in displist:               # 第一个小部件插入数据
+#        listb.insert(0,item)
+#     
+#    listb.pack()                        # 将小部件放置到主窗口中
+#    window.mainloop() 
+
       
         
-# def getNeighbours(self, input_1): 
-#    alive_count = 0
-#    for x_of in xrange(-1, 2): 
-#      for y_of in xrange(-1, 2): 
-#        c_x, c_y = input_1.x + x_of, input_1.y + y_of 
-#        if ((c_x, c_y) == cell_obj.point) or \ 
-#          (c_x < 0 or c_x >= WORLD_WIDE) or \ 
-#          (c_y < 0 or c_y >= WORLD_HIGH): 
-#          '''''排除自身和越界的点'''
-#          continue
-#        if self.world[c_x][c_y].is_alive: 
-#          alive_count += 1
-#    return alive_count 
 
         
